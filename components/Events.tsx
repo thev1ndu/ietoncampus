bb8e88cd5a2c93cba22ac7bf845ef2e878995bfa
hesxo <hasal.20241903@iit.ac.lk>
2025-07-11 21:42:21 +0530

    UPDATE

--- components/Events.tsx
+++ components/Events.tsx
@@ -1,18 +1,28 @@
 import { getAllEvents } from "@/lib/events";
 import EventCard from "./EventCard";
+import SectionHeading from "./section-heading";
 
 export default async function Events() {
   const events = await getAllEvents();
 
   if (!events.length) {
-    return <p>No events found.</p>;
+    return <p className="text-center">No events found.</p>;
   }
 
   return (
-    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {events.map((event) => (
-        <EventCard key={event.slug} event={event} />
-      ))}
+    <div className="mb-8 scroll-mt-[140px]" id="events">
+      <h3 className="text-xs sm:text-sm font-medium mb-3 sm:mb-4 uppercase tracking-wide text-muted-foreground text-center">
+        Our Events
+      </h3>
+      <SectionHeading>
+        Discover Our Latest Events, Workshops & Innovation Showcases
+      </SectionHeading>
+
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-3 gap-6 justify-center">
+        {events.map((event) => (
+          <EventCard key={event.slug} event={event} />
+        ))}
+      </div>
     </div>
   );
 }
